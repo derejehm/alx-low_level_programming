3c729faadf5d58927a83b3a94a6d9e20b2730b7d
Dereje Hailemariam <hailemariam013@gmail.com>
2022-04-07 05:35:58 -0700

    betty : remove wihte space

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,8 +4,11 @@
 
 #include <stdio.h>
 
+/*
+* main function
+*/
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle"); /* display the text */
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0); /* return 0*/
 }
