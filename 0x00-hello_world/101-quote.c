81b374dae570cb1af5d61984ae0fcf90365849ed
Dereje Hailemariam <hailemariam013@gmail.com>
2022-04-07 08:57:32 -0700

    101-quote.c script added

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,10 @@
+#include <stdlib.h>
+/**
+*main - main function
+*Return: 1- to exit
+*/
+int main(void)
+{
+	system("echo 'and that piece of art is useful\" - Dora Korpar, 2015-10-19'");
+	return (1);
+}
