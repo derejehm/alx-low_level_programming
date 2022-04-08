b8362f4665848733db884d41fd73d6bbc90cd167
Dereje Hailemariam <hailemariam013@gmail.com>
2022-04-08 11:14:25 -0700

    6-print_numberz.c added

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+*main - main function
+*Return: 0- to exit
+*/
+int main(void)
+{
+	int ch;
+
+	for (ch = 48; ch <= 57; ch++)
+		putchar(ch);
+	putchar('\n');
+	return (0);
+}
